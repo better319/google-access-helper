6d77cec4329b06368b6ad9e7757ba5499608fcac
haotian-wang <haotian-wang@foxmail.com>
2018-06-08 14:16:35 +0800

    更新至 2.3.0

--- theme/welcome.js
+++
@@ -1,5 +0,0 @@
-if(window.navigator.userAgent.match(/(LBBROWSER)/i)){
-	document.getElementById("sethomepage").setAttribute("href","http://www.ggfwzs.com/setliebao/");
-}else if(window.navigator.userAgent.match(/(BIDUBrowser)/i)){
-	document.getElementById("sethomepage").setAttribute("href","http://www.ggfwzs.com/setbaidu/");
-}
